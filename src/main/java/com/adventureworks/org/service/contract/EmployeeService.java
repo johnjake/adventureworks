24a1ae99e9db78cd1031537fd1e40199a7c98b95
Talledo <johnjake.talledo2000@gmail.com>
2024-01-10 15:41:52 +0800

    fixed endpoint

--- src/main/java/com/adventureworks/org/service/contract/EmployeeService.java
+++ src/main/java/com/adventureworks/org/service/contract/EmployeeService.java
@@ -2,7 +2,6 @@ package com.adventureworks.org.service.contract;
 
 import com.adventureworks.org.model.Employee;
 import com.adventureworks.org.model.aggregate.EmployeeStateCount;
-import com.adventureworks.org.repository.EmployeeRepository;
 import reactor.core.publisher.Flux;
 
 public interface EmployeeService {
