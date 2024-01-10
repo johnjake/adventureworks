0bc369da433c16879bdd632179dbea6ed8739f04
Talledo <johnjake.talledo2000@gmail.com>
2024-01-10 14:18:37 +0800

    listEmployees by city

---
+++ src/main/java/com/adventureworks/org/service/contract/EmployeeService.java
@@ -0,0 +1,9 @@
+package com.adventureworks.org.service.contract;
+
+import com.adventureworks.org.model.Employee;
+import com.adventureworks.org.repository.EmployeeRepository;
+import reactor.core.publisher.Flux;
+
+public interface EmployeeService {
+    Flux<Employee> findEmployeesByCity(String city);
+}
---
+++ src/main/java/com/adventureworks/org/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,21 @@
+package com.adventureworks.org.service.impl;
+
+import com.adventureworks.org.model.Employee;
+import com.adventureworks.org.repository.EmployeeRepository;
+import com.adventureworks.org.service.contract.EmployeeService;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    @Override
+    public Flux<Employee> findEmployeesByCity(String city) {
+        return employeeRepository.findEmployeesByCity(city);
+    }
+}
